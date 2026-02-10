c627b7103b97bbfb094489d91fcba336b728da23
Benjamin Idama <edafebenjamin2@gmail.com>
2026-02-10 16:47:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 40 }}>
+      <h1>Hi, I’m Ben 👋</h1>
+      <h2>Frontend React Developer</h2>
+
+      <h3>Projects</h3>
+    </div>
+  );
+}
